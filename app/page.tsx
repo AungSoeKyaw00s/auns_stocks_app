507325bebf4b3af836ada8d154e4fca39821dff7
AungSoeKyaw00s <aungsoekyaw2017@gmail.com>
2025-10-05 16:21:22 +1100

    added commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+const page = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+    <Button>click me</Button>
+    </div>
+    
+  )
+}
+
+export default page
